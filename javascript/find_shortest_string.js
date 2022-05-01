95bfdc9c2e39501e81a9d61fc8d139bd051e690c
justinctollison <justinctollison@gmail.com>
2022-05-01 16:45:15 -0700

    Completed assignment.

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,9 @@
 function findShortestString(arr) {
   // type your code here
+  let shortestString = arr.reduce(function(a, b) {
+    return a.length <= b.length ? a: b;
+  })
+  return shortestString
 }
 
 if (require.main === module) {
@@ -24,3 +28,6 @@ module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+//iterate over the array, find the string with the shortest amount of characters, return this string.
+//count the numbers of characters in each array element, return the minimum element
+//use reduce method on each element of the array that uses a reducer function comparing each elements length and using it
